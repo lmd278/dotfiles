4c288658e01eb7cf851a5c3ffe970accc1e30285
lmd-278 <palered548@gmail.com>
2021-07-29 22:30:21 +0700

    Fix more bugs

--- .packages/aur.sh
+++ .packages/aur.sh
@@ -6,6 +6,8 @@ yay -S neovim-git
 yay -S optimus-manager
 yay -S optimus-manager-qt
 yay -S polybar
+yay -S teams
 yay -S ttf-ms-fonts
 yay -S ttf-tahoma
 yay -S ttf-vista-fonts
+yay -S visual-studio-code-bin
