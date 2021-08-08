7f7fc49b67c290c16b0e071cd7184cb7a5491ecf
lmd-278 <palered548@gmail.com>
2021-08-08 09:42:31 +0700

    Major changes

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -7,4 +7,4 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch polybar
-polybar -q main -c '~/.config/polybar/config.ini' &
+polybar -q main -c '$HOME/.config/polybar/config.ini' &
