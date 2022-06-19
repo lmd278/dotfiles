ae54b8ce045fd9cb269ccd61310a8546413cdfc3
lmd278 <palered548@gmail.com>
2022-06-19 19:47:04 +0700

    Fix LSP

--- .config/nvim/lua/user/illuminate.lua
+++ .config/nvim/lua/user/illuminate.lua
@@ -1,5 +1,8 @@
 -- vim.g.Illuminate_delay = 0
 -- vim.g.Illuminate_highlightUnderCursor = 0
 vim.g.Illuminate_ftblacklist = {"alpha", "NvimTree"}
-vim.api.nvim_set_keymap("n", "<A-n>", '<cmd>lua require"illuminate".next_reference{wrap=true}<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<A-p>", '<cmd>lua require"illuminate".next_reference{reverse=true,wrap=true}<CR>', { noremap = true, silent = true })
+
+-- Custom keymaps
+local keymap = vim.keymap.set
+keymap("n", "<A-n>", '<cmd>lua require"illuminate".next_reference{wrap=true}<CR>')
+keymap("n", "<A-p>", '<cmd>lua require"illuminate".next_reference{reverse=true,wrap=true}<CR>')
